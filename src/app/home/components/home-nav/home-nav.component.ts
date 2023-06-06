b1f89ba6070974e48d41d1e8c2279293986733a3
Neider García <yesid19999@gmail.com>
2023-06-06 18:18:34 -0500

    add nav component

---
+++ src/app/home/components/home-nav/home-nav.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-home-nav',
+  templateUrl: './home-nav.component.html',
+  styleUrls: ['./home-nav.component.scss']
+})
+export class HomeNavComponent implements OnInit {
+  
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+      /* this.items = [
+          {
+              label: 'File',
+              icon: 'pi pi-fw pi-file',
+              items: [
+                  {
+                      label: 'New',
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {
+                              label: 'Bookmark',
+                              icon: 'pi pi-fw pi-bookmark'
+                          },
+                          {
+                              label: 'Video',
+                              icon: 'pi pi-fw pi-video'
+                          }
+                      ]
+                  },
+                  {
+                      label: 'Delete',
+                      icon: 'pi pi-fw pi-trash'
+                  },
+                  {
+                      separator: true
+                  },
+                  {
+                      label: 'Export',
+                      icon: 'pi pi-fw pi-external-link'
+                  }
+              ]
+          },
+      ]; */
+  }
+
+  login(): void{
+
+  }
+
+  register(): void{
+
+  }
+}
